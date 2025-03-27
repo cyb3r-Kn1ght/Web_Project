e104e21e18e9ee1ce03d5d27c05e909e38ecb5e2
OB2UKT <23520896@gm.uit.edu.vn>
2025-03-27 17:03:11 +0700

    Suon cua FE

--- frontend/src/lib/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'; // cài đặt thư viện HTP client
-
-//mọi request HTTP sẽ được đính kèm các thông tin xác thực, được gửi đến đường dẫn http://localhost:3001/<relative URL>
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api",
-    withCredentials: true,
-}); 
