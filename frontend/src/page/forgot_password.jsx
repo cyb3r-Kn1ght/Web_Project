9e8ca2057143bdda8342fdfc8ad1f274214c90dc
OB2UKT <23520896@gm.uit.edu.vn>
2025-03-27 23:53:17 +0700

    Them cac components

---
+++ frontend/src/page/forgot_password.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import "../style/login/login.css";
+import Input_Field from "../components/login/Input_Fields.jsx";
+
+function Forgot_Password() {
+  return (
+    <>
+      <div className="forms-container">
+        <div className="signin-signup"></div>
+        <form action="#" className="sign-in-form">
+          <h2 className="title">Forgot Password</h2>
+
+          {/* su dung component vao thuc te */}
+          <Input_Field
+            icon="mail"
+            type="email"
+            placeholder="Email"
+            // value={formData.email} // doan nay can BE check lai, thieu gi do ma web k chay dc tam thoi bo comment
+            // onChange={(e) =>
+            //   setFormData({ ...formData, email: e.target.value })
+            // }
+          />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Forgot_Password;
--- frontend/src/page/login.jsx
+++ frontend/src/page/login.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
+// import { Link } from "react-router-dom";
 import { initLoginHandlers } from "../feature/login/login.js";
 import GoogleIcon from "../assets/login/google.svg";
 import FacebookIcon from "../assets/login/facebook.svg";
 import "../style/login/login.css";
-function App() {
+import Input_Field from "../components/login/Input_Fields.jsx";
+function Login() {
   useEffect(() => {
     // Gọi hàm chuyển cảnh sau khi component render xong
     initLoginHandlers();
@@ -11,23 +13,40 @@ function App() {
 
   return (
     <>
-      <div class="container">
-        <div class="forms-container">
-          <div class="signin-signup">
-            <form action="#" class="sign-in-form">
-              <h2 class="title">Sign in</h2>
-              <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Username" />
-              </div>
-              <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Password" />
-              </div>
-              <input type="submit" value="Login" class="btn solid" />
-              <p class="social-text">Or sign in with social platforms</p>
-              <div class="social-media">
-                <a href="#" class="social-icon">
+      <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+
+              {/* su dung component vao thuc te */}
+              <Input_Field
+                icon="mail"
+                type="email"
+                placeholder="Email"
+                // value={formData.email} // doan nay can BE check lai, thieu gi do ma web k chay dc tam thoi bo comment
+                // onChange={(e) =>
+                //   setFormData({ ...formData, email: e.target.value })
+                // }
+              />
+
+              <Input_Field
+                icon="key"
+                type="password"
+                placeholder="Password"
+                // value={formData.password} // doan nay can BE check lai
+                // onChange={(e) =>
+                //   setFormData({ ...formData, password: e.target.value })
+                // }
+              />
+
+              <p className="forget-password">
+                {/* Forget password <Link to="/forgot-password">click here!</Link> */}
+              </p>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
                   <img
                     src={FacebookIcon}
                     alt="FacebookIcon"
@@ -35,7 +54,7 @@ function App() {
                   />
                 </a>
 
-                <a href="#" class="social-icon">
+                <a href="#" className="social-icon">
                   <img
                     src={GoogleIcon}
                     alt="GoogleIcon"
@@ -44,24 +63,54 @@ function App() {
                 </a>
               </div>
             </form>
-            <form action="#" class="sign-up-form">
-              <h2 class="title">Sign up</h2>
-              <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Username" />
-              </div>
-              <div class="input-field">
-                <i class="fas fa-envelope"></i>
-                <input type="email" placeholder="Email" />
-              </div>
-              <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Password" />
-              </div>
-              <input type="submit" class="btn" value="Sign up" />
-              <p class="social-text">Or sign up with social platforms</p>
-              <div class="social-media">
-                <a href="#" class="social-icon">
+
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+
+              <Input_Field
+                icon="account_circle"
+                type="text"
+                placeholder="Username"
+                // value={formData.username} // doan nay can BE check lai, thieu gi do ma web k chay dc tam thoi bo comment
+                // onChange={(e) =>
+                //   setFormData({ ...formData, username: e.target.value })
+                // }
+              />
+
+              <Input_Field
+                icon="mail"
+                type="email"
+                placeholder="Email"
+                // value={formData.email} // doan nay can BE check lai, thieu gi do ma web k chay dc tam thoi bo comment
+                // onChange={(e) =>
+                //   setFormData({ ...formData, email: e.target.value })
+                // }
+              />
+
+              <Input_Field
+                icon="key"
+                type="password"
+                placeholder="Password"
+                // value={formData.password} // doan nay can BE check lai
+                // onChange={(e) =>
+                //   setFormData({ ...formData, password: e.target.value })
+                // }
+              />
+
+              <Input_Field
+                icon="verified_user"
+                type="password"
+                placeholder="Confirm Password"
+                // value={formData.password} // doan nay can BE check lai
+                // onChange={(e) =>
+                //   setFormData({ ...formData, password: e.target.value })
+                // }
+              />
+
+              <input type="submit" className="btn" value="Sign up" />
+              <p className="social-text">Or sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
                   <img
                     src={FacebookIcon}
                     alt="FacebookIcon"
@@ -69,7 +118,7 @@ function App() {
                   />
                 </a>
 
-                <a href="#" class="social-icon">
+                <a href="#" className="social-icon">
                   <img
                     src={GoogleIcon}
                     alt="GoogleIcon"
@@ -81,22 +130,22 @@ function App() {
           </div>
         </div>
 
-        <div class="panels-container">
-          <div class="panel left-panel">
-            <div class="content">
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
               <h3>New here ?</h3>
               <p>Create account to chat with me!</p>
-              <button class="btn transparent" id="sign-up-btn">
+              <button className="btn transparent" id="sign-up-btn">
                 Sign up
               </button>
             </div>
-            <img src="img/log.svg" class="image" alt="" />
+            <img src="img/log.svg" className="image" alt="" />
           </div>
-          <div class="panel right-panel">
-            <div class="content">
+          <div className="panel right-panel">
+            <div className="content">
               <h3>One of us ?</h3>
               <p>Welcome back!</p>
-              <button class="btn transparent" id="sign-in-btn">
+              <button className="btn transparent" id="sign-in-btn">
                 Sign in
               </button>
             </div>
@@ -107,4 +156,4 @@ function App() {
   );
 }
 
-export default App;
+export default Login;
