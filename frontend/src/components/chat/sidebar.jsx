0d18933ab5e6635770c94ea63d0d2516a2e50a9d
T1HL <luonghoanglong0302@gmail.com>
2025-05-25 12:13:43 +0700

    Chỉnh responsive

--- frontend/src/components/chat/sidebar.jsx
+++ frontend/src/components/chat/sidebar.jsx
@@ -10,7 +10,27 @@ import { faBars, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 // component này đại diện cho sidebar, bao gồm header, nội dung và footer
 const Sidebar = ({ celebs, selectedCeleb, handleSelect }) => {
   // State để lưu trạng thái thu gọn hoặc mở rộng sidebar
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(
+    () => window.matchMedia('(max-width: 768px)').matches
+  );
+  useEffect(() => {
+    // Tạo một đối tượng media query
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    // Hàm listener để cập nhật state 'collapsed' khi có sự thay đổi
+    const handleResize = () => {
+      setCollapsed(mediaQuery.matches);
+    };
+
+    // Thêm listener vào media query
+    mediaQuery.addEventListener('change', handleResize);
+
+    // Dọn dẹp listener khi component bị unmount (rất quan trọng để tránh rò rỉ bộ nhớ)
+    return () => {
+      mediaQuery.removeEventListener('change', handleResize);
+    };
+  }, []); // Mảng rỗng [] đảm bảo effect này chỉ chạy 1 lần khi component mount
+
   // Hàm để thay đổi trạng thái thu gọn hoặc mở rộng sidebar
   const toggleSidebar = () => setCollapsed(!collapsed);
   return (
