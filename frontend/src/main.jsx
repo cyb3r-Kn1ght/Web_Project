04fdccb88f18e4cb61529b28872149ccbc27f20b
Mai2el <ndtnetdautruong@gmail.com>
2025-05-26 16:14:52 +0700

    backup lần 22

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import { useAuthStore } from "./store/useAuthStore";
 import React from "react";
-const Init = () => {
-  const checkAuth = useAuthStore((state) => state.checkAuth);
 
-  useEffect(() => {
-    checkAuth(); // Gọi ngay khi frontend khởi chạy (sau Google redirect)
-  }, [checkAuth]);
-
-  return <App />;
-};
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
