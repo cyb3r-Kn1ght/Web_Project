d541904a830a35e7a8720ad5340f62244a16523c
Mai2el <ndtnetdautruong@gmail.com>
2025-05-26 16:06:22 +0700

    test lần 11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+import { useAuthStore } from "./store/useAuthStore";
 import React from "react";
+const Init = () => {
+  const checkAuth = useAuthStore((state) => state.checkAuth);
 
+  useEffect(() => {
+    checkAuth(); // Gọi ngay khi frontend khởi chạy (sau Google redirect)
+  }, [checkAuth]);
+
+  return <App />;
+};
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
