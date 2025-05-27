c1d99ea62e30729c2214ce1e8a693d0af18b2651
Mai2el <ndtnetdautruong@gmail.com>
2025-05-27 09:05:07 +0700

    backup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,13 @@
-import { StrictMode, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import React from "react";
-import { useAuthStore } from "./store/useAuthStore";
-
-const Init = () => {
-  const checkAuth = useAuthStore((state) => state.checkAuth);
-  
-  useEffect(() => {
-    checkAuth(); // gọi ngay khi app khởi chạy
-  }, [checkAuth]);
-
-  return <App />;
-};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Init />
+      <App />
     </Router>
   </StrictMode>
 );
