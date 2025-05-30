d63460f42c5d6a80c956fe513a5bb4cdcae3d6b3
T1HL <luonghoanglong0302@gmail.com>
2025-05-30 15:17:44 +0700

    Tối ưu hiệu suất

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,28 @@
-import { lazy, Suspense } from "react";
+import Home from "./page/web_page/Home.jsx";
+import Login from "./page/login/login.jsx";
+import Chat from "./page/chat/chat.jsx";
+import Payment from "./page/payment/payment.jsx";
+
+
+import { useAuthStore } from "./store/useAuthStore";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { useAuthStore } from "./store/useAuthStore";
-
-// Lazy load
-const Home = lazy(() => import("./page/web_page/Home.jsx"));
-const Login = lazy(() => import("./page/login/login.jsx"));
-const Chat = lazy(() => import("./page/chat/chat.jsx"));
-const Payment = lazy(() => import("./page/payment/payment.jsx"));
-
 function App() {
   const { authUser } = useAuthStore();
-
   return (
     <>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/auth/login"
-            element={authUser ? <Navigate to="/chat" /> : <Login />}
-          />
-          <Route
-            path="/chat"
-            element={authUser ? <Chat /> : <Navigate to="/auth/login" />}
-          />
-          <Route
-            path="/payment"
-            element={authUser ? <Payment /> : <Navigate to="/auth/login" />}
-          />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={authUser ? <Navigate to="/chat" /> : <Login />} />
+        <Route 
+          path="/chat" 
+          element={authUser ? <Chat /> : <Navigate to="/auth/login" />} 
+        />
+        <Route 
+          path="/payment" 
+          element={authUser ? <Payment /> : <Navigate to="/auth/login" />}
+        />
+      </Routes>
 
       <Toaster />
     </>
