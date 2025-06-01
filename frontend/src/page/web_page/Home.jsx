6319eff7bad3f2d35913728c21ed822db22ea995
Mai2el <ndtnetdautruong@gmail.com>
2025-06-01 14:15:30 +0700

    tối ưu đi mà

--- frontend/src/page/web_page/Home.jsx
+++ frontend/src/page/web_page/Home.jsx
@@ -1,7 +1,6 @@
 import Navbar from "../../components/web_page/navbar.jsx";
 import  { lazy, Suspense, useEffect } from 'react';
 import "../../style/web_page/Home_module.css";
-import "../../style/web_page/navbar.css";
 
 const Tittle = lazy(() => import('../../components/web_page/tittle.jsx'));
 const ContainerButton = lazy(() => import('../../components/web_page/container-button.jsx'));
