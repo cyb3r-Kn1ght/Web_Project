53aca7944392a8b33a421774e67ab3f76d18b1ce
T1HL <luonghoanglong0302@gmail.com>
2025-06-02 16:10:43 +0700

    Điều chỉnh interview

--- frontend/src/components/web_page/tittle.jsx
+++ frontend/src/components/web_page/tittle.jsx
@@ -2,7 +2,6 @@
 import "../../style/web_page/tittle.css";
 function title() {
   return (
-    
     <section className="Tittle-section">
       <h1 className="MainTittle">AI ChatBot</h1>
       <h2 className="debcribe">Trò chuyện cùng các Idol</h2>
